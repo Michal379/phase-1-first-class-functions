1635445fec0ab4d40b3aa1bf1bb6ee1bfa0a51db
Michal <khakaimic4@gmail.com>
2023-03-17 18:41:46 +0300

    completed lab

---
+++ index.js
@@ -0,0 +1,14 @@
+const x = d();
+function receivesAFunction(x){
+    x();
+}
+//
+function returnsANamedFunction() {
+    return receivesAFunction;
+}
+//
+function returnsAnAnonymousFunction(){
+    return function (){
+        console.log("I am anonymous");
+    }
+}
